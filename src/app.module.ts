a6a1694d2e47c7cc00d624a82f7e02177a591ebe
Thiago Lopes <thiagoolopezz@gmail.com>
2023-10-02 13:17:17 -0300

    71 Crud Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    UsersModule
   ],
   controllers: [
     AppController
   ],
   providers: [
-    AppService,
-    PrismaService
+    AppService
   ],
 })
 export class AppModule { }
