599544549364348c06b1a7b4072c8c1212a38774
Thiago Lopes <thiagoolopezz@gmail.com>
2023-11-21 13:46:07 -0300

    82 a 86  Interceptors gerais e Prisma Interceptor

---
+++ src/common/errors/interceptors/conflict.interceptor.ts
@@ -0,0 +1,25 @@
+import {
+  CallHandler,
+  ConflictException,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor
+} from '@nestjs/common';
+import { Observable, catchError } from 'rxjs';
+import { ConflictError } from '../types/ConflictError';
+
+
+@Injectable()
+export class ConflictInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError(error => {
+        if (error instanceof ConflictError) {
+          throw new ConflictException(error.message);
+        } else {
+          throw error;
+        }
+      }),
+    );
+  }
+}
---
+++ src/common/errors/utils/handler-database-errors.ts
@@ -0,0 +1,18 @@
+import { DatabaseError } from "../types/DatabaseError";
+import { PrismaClientError } from "../types/PrismaClientError";
+import { UniqueConstraintError } from "../types/UniqueConstraintError";
+
+enum PrismaErrors {
+  UniqueConstraintFail = 'P2002'
+}
+
+export const handleDatabaseErrors = (e: PrismaClientError): Error => {
+  switch (e.code) {
+    case PrismaErrors.UniqueConstraintFail:
+      return new UniqueConstraintError(e);
+
+
+    default:
+      return new DatabaseError(e.message);
+  }
+}
